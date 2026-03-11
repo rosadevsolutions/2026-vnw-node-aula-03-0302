0164408a78ab4630a8dcad8ed82a0f06ab735417
Romulo Rosa | FullStack | Rosadev Solutions <rosadevsolutions@gmail.com>
2026-03-11 17:55:06 -0300

    server.js post

--- server.js
+++ server.js
@@ -23,6 +23,25 @@ app.get("/usuarios", (req, res) => {
   res.status(200).json(usuarios);
 });
 
+// 09 - Rotas para adicionar/cadastrar novos usuarios
+app.post("/usuarios", (req, res) => {
+  // 10 - Criar variavel para definir dados a serem enviados no body do request, no caso nome e idade. O id não é inserido porque será auto-incrementado somente para o cadastro em novos usuarios
+  const { nome, idade } = req.body;
+
+  // 11 - Criar variavel armazenar um padrão de envio das infos
+  const novoUsuario = {
+    id: usuarios.length + 1,
+    nome,
+    idade,
+  };
+
+  // 12 - Enviar os dados organizados do novo usuatio para o array de usuarios
+  usuarios.push(novoUsuario);
+
+  // 13 - Criando uma response para caso o envio de novoUsuario ocorra com sucesso exiba o status code
+  res.status(201).json(novoUsuario);
+});
+
 // 08 - Inicializar app.
 app.listen(PORT, () => {
   console.log(`Servidor rodando na http://localhost:${PORT}/usuarios`);
